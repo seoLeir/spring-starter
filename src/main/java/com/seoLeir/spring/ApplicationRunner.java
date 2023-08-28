d391fae2afa9effbd4dbac84339e1b7f1269be19
Miralim Mirzayev <mirzayevmiralim28@gmail.com>
2023-08-28 23:55:55 +0500

    Add @OpenApiDefinition annotation on the ApplicationRunner class

--- src/main/java/com/seoLeir/spring/ApplicationRunner.java
+++ src/main/java/com/seoLeir/spring/ApplicationRunner.java
@@ -14,6 +14,7 @@ import java.time.LocalDate;
 
 @ConfigurationPropertiesScan
 @SpringBootApplication
+@OpenAPIDefinition
 public class ApplicationRunner {
     public static void main(String[] args) {
         ConfigurableApplicationContext context = SpringApplication.run(ApplicationRunner.class, args);
