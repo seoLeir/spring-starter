e88bd9c6a8ac6573d66448616a332c56aa51a8ef
Miralim Mirzayev <mirzayevmiralim28@gmail.com>
2023-09-10 01:58:04 +0500

    Add auto security authentication in integration tests with spring-secirity-test

--- src/test/java/com/seoLeir/spring/integration/IntegrationTestBase.java
+++ src/test/java/com/seoLeir/spring/integration/IntegrationTestBase.java
@@ -2,6 +2,7 @@ package com.seoLeir.spring.integration;
 
 import com.seoLeir.spring.integration.annotation.IT;
 import org.junit.jupiter.api.BeforeAll;
+import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.context.DynamicPropertyRegistry;
 import org.springframework.test.context.DynamicPropertySource;
 import org.springframework.test.context.jdbc.Sql;
@@ -11,6 +12,7 @@ import org.testcontainers.containers.PostgreSQLContainer;
 @Sql({
         "classpath:sql/data.sql"
 })
+@WithMockUser(username = "test@gmail.com", password = "test", authorities = {"ADMIN", "USER"})
 public abstract class IntegrationTestBase {
 
     private static final PostgreSQLContainer<?> CONTAINER = new PostgreSQLContainer<>("postgres:15.0");
